fa91eb54a5733cabe9b50c0bdb5f342e89f9e6a4
JijoNasrun <jijohakim@gmail.com>
2016-11-27 23:24:40 +0800

    Sample Image For List

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { ProfilePage } from '../profile/profile'
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public http: Http) {
   }
 
   login(){
   	this.navCtrl.push(ProfilePage);
   	this.navCtrl.setRoot(ProfilePage);
   }
-
 }
